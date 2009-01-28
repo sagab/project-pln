a38642fcadeec62e9278cd8a15eb1a677bff925b
gabrim.san <gabrim.san@24813a74-ed1b-11dd-9059-71fac19e68c6>
2009-01-28 13:59:07 +0000

    - XML parser + writer now

--- xmlparse.py
+++ xmlparse.py
@@ -3,8 +3,32 @@
 # version but DOM is simpler, in my opinion.
 
 from xml.dom import minidom
-doc = minidom.parse('xml/1.xml')
-people = doc.getElementsByTagName('Person')
+from xml.dom.minidom import getDOMImplementation
+
+doc1 = minidom.parse('xml/1.xml')
+people = doc1.getElementsByTagName('Person')
+
+# creating an XML
+imp = getDOMImplementation()
+doc2 = imp.createDocument(None, 'names', None)
+top = doc2.documentElement
 
 for a in people:
-	print a.attributes['nickname'].value
+	v = a.attributes['nickname'].value
+	
+	print v
+	
+	# really need to create document structure Element, Node, Attribute, etc.
+	nod = doc2.createElement('name')
+	top.appendChild(nod)
+	tn = doc2.createTextNode(v)
+	nod.appendChild(tn);
+
+# writes content of doc2 DOM from memory to a file
+file = open('xml/names.xml','w')
+doc2.writexml(file,'  ','  ','\n')
+file.close()
+
+#clean up mem
+doc1.unlink()
+doc2.unlink()
