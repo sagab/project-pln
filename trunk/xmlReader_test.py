d5e2d5ecf71822e165c89863ef309e0b0d9c7852
dumitrescu.stefan <dumitrescu.stefan@24813a74-ed1b-11dd-9059-71fac19e68c6>
2009-02-10 10:53:22 +0000

    update test la xmlreader

--- trunk/xmlReader_test.py
+++ trunk/xmlReader_test.py
@@ -1,7 +1,40 @@
 import xmlReader
+from general import *
+from nltk.corpus import wordnet as wn
+
+
+print '-------------------------------------------------'
+#dictionar
+freq_dict = {}
+
 
 r = xmlReader.XMLReader()
 r.read('xml/4.xml')
 
 for msg in r.doc:
-	print msg.id,":",msg.refid,":",msg.nick," : ",msg.text
+	#print msg.id,":",msg.refid,":",msg.nick," : ",stripStopWords_split(msg.text)," || ",msg.text
+	updateFreqList(stripStopWords_split(msg.text),freq_dict)
+
+
+print ''
+print ''
+
+freq_dict = sort_freq_dict(freq_dict,50)
+print freq_dict
+word = freq_dict[0]
+word2 = freq_dict[7]
+print word[0];
+
+
+
+print 'START'
+
+w1 = wn.synsets(word[0])[0];
+w2 = wn.synsets(word2[0])[0];
+print w1,w2
+print w1.lch_similarity(w2);
+
+
+print ''
+#print freq_dict
+print 'END'
