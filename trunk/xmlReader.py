d1c74036d11c4e7c820aa5641c6b9d7e614cade4
anamch <anamch@24813a74-ed1b-11dd-9059-71fac19e68c6>
2009-02-12 22:14:16 +0000

    adaugat o functie in xmlReader pentru citirea fisierului cu post

--- trunk/xmlReader.py
+++ trunk/xmlReader.py
@@ -34,6 +34,21 @@ class XMLReader:
 			ut.id = utterance_nodes[0].attributes['genid'].value
 			ut.refid = utterance_nodes[0].attributes['ref'].value
 			self.doc.append(ut)
+
+	def readPOST(self,filename):
+		self.doc = []		
+		self.xmldoc = minidom.parse(filename)
+		aux_node = self.xmldoc.getElementsByTagName('POST')
+		turns_node = aux_node[0].getElementsByTagName('TagUt')
+		for turn_node in turns_node:
+			ut = Utterance()
+			ut.id = turn_node.attributes['id'].value
+			rc = ""
+			if turn_node.hasChildNodes():
+				if turn_node.firstChild.nodeType == turn_node.TEXT_NODE:
+					rc = rc + turn_node.firstChild.data
+			ut.text = rc
+			self.doc.append(ut)
 		
 
 	def close(self):
